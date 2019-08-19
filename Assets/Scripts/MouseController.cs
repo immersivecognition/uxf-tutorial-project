da946b7229be3c6467c430ba34371e2be5d8af58
Jack Brookes <jbrookes187@gmail.com>
2019-08-19 11:44:30 -0700

    part 2 complete

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    // reference to the camera we project from
+    public Camera projectionCamera;    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get mouse position in pixels
+        Vector3 mousePos = Input.mousePosition;
+
+        // calculate world position of mouse
+        Vector3 worldPos = projectionCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0));
+
+        // fix the height of our calculated position to match the current y position of the object
+        // that way it moves only in 2D (XZ plane)
+        worldPos.y = transform.position.y;
+
+        // update position of cursor to our newly calculated position
+		transform.position = worldPos;
+    }
+}
