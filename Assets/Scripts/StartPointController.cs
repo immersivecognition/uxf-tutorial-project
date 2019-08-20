09377e5dac55434182bee8baa25408f26f61c55a
Jack Brookes <jbrookes187@gmail.com>
2019-08-19 17:09:46 -0700

    debug log on trigger

---
+++ Assets/Scripts/StartPointController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPointController : MonoBehaviour
+{
+    /// OnTriggerEnter is called when the Collider 'other' enters the trigger.
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("A collider entered the trigger!");
+    }
+
+    /// OnTriggerExit is called when the Collider 'other' has stopped touching the trigger.
+    void OnTriggerExit(Collider other)
+    {      
+        Debug.Log("A collider exited the trigger!");
+    }
+}
