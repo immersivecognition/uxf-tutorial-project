ff2da33ee422199768dd7b0891f8c7544aa344a6
Jack Brookes <jbrookes187@gmail.com>
2019-08-19 17:24:15 -0700

    color change

--- Assets/Scripts/StartPointController.cs
+++ Assets/Scripts/StartPointController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class StartPointController : MonoBehaviour
 {
+    // define 3 public variables - we can then assign their color values in the inspector.
+    public Color red;
+    public Color amber;
+    public Color green;
+
+    // reference to the material we want to change the color of.
+    Material material;
+
+    /// Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        // get the material that is used to render this object (via the MeshRenderer component)
+        material = GetComponent<MeshRenderer>().material;
+    }
+
     /// OnTriggerEnter is called when the Collider 'other' enters the trigger.
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("A collider entered the trigger!");
+        material.color = amber;
     }
 
     /// OnTriggerExit is called when the Collider 'other' has stopped touching the trigger.
     void OnTriggerExit(Collider other)
     {      
-        Debug.Log("A collider exited the trigger!");
+        material.color = red;        
     }
 }
