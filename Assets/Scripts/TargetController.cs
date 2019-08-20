4d400f420b8acd575298f5358ebe58594d637d6d
Jack Brookes <jbrookes187@gmail.com>
2019-08-20 06:15:34 -0700

    part 4 complete

---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    // reference to the AudioClip we want to play on trigger enter.
+    public AudioClip collectSound;
+
+    /// OnTriggerEnter is called when the Collider 'other' enters the trigger.
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Cursor")
+        {
+            // disable the whole GameObject
+            gameObject.SetActive(false);
+
+            // play the collect sound (at the same position as the target, 100% volume)
+            AudioSource.PlayClipAtPoint(collectSound, transform.position, 1.0f);
+        }
+    }
+}
