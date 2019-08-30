74ba441b13337c21942ba7086018241d1668a757
Jack Brookes <jbrookes187@gmail.com>
2019-08-30 14:18:57 -0700

    part 10 complete

--- Assets/Scripts/TaskAreaController.cs
+++ Assets/Scripts/TaskAreaController.cs
@@ -18,7 +18,6 @@ public class TaskAreaController : MonoBehaviour
 
             // calculate the angle from the target (forward direction) to the cursor (about the up axis)
             float angle = Vector3.SignedAngle(targetDir, cursorPos, Vector3.up);
-            Debug.Log(angle);
 
             // store the result in the current trial - this will then be saved in the trial_results csv
             session.CurrentTrial.result["angle"] = angle;
--- Assets/Scripts/VisuoMotorRotation.cs
+++ Assets/Scripts/VisuoMotorRotation.cs
@@ -8,13 +8,41 @@ public class VisuoMotorRotation : MonoBehaviour
     public Session session;
     public Transform hiddenCursor;
     public Transform startPoint;
-
+    
+    MeshRenderer cursorMR;
     float vmrAngle = 0f;
+    bool clamped = false;
+
+    void Awake()
+    {
+        // get the mesh renderer attached to this component
+        // could also be done with public variable
+        cursorMR = GetComponent<MeshRenderer>();
+    }
 
     void Update()
     {
-        // update the position of this object
-        transform.position = hiddenCursor.position;
+        // we don't want to have any intervention when moving back to the start point
+        // so if we aren't in a trial let's just show the cursor normally
+        if (!session.hasInitialised | !session.InTrial)
+        {
+            transform.position = hiddenCursor.position;
+            return; // exit the function early
+        }
+
+        if (clamped)
+        {
+            // calculate distance from center
+            float dist = (hiddenCursor.position - startPoint.position).magnitude;
+            // move towards target with calculated distance (i.e. with 0 error)
+            transform.position = startPoint.position + Vector3.forward * dist;
+        }
+        else
+        {
+            // update the position of this object
+            transform.position = hiddenCursor.position;
+        }
+
         transform.RotateAround(startPoint.position, Vector3.up, vmrAngle);
     }
 
@@ -23,5 +51,9 @@ public class VisuoMotorRotation : MonoBehaviour
     {
         // use settings.Get*() to access settings (independent variables)
         vmrAngle = session.CurrentTrial.settings.GetFloat("vmr_angle");
+        clamped = session.CurrentTrial.settings.GetBool("clamped_error");
+
+        // enable/disable cursor mesh renderer as needed
+        cursorMR.enabled = session.CurrentTrial.settings.GetBool("online_feedback");
     }
 }
