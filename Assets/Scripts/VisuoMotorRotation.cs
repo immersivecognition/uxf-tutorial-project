18377ffb567244ace392613de838d215fe2282c8
Jack Brookes <jbrookes187@gmail.com>
2019-08-30 13:29:46 -0700

    part 8 complete

---
+++ Assets/Scripts/VisuoMotorRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UXF;
+
+public class VisuoMotorRotation : MonoBehaviour
+{
+    public Session session;
+    public Transform hiddenCursor;
+    public Transform startPoint;
+
+    float vmrAngle = 0f;
+
+    void Update()
+    {
+        // update the position of this object
+        transform.position = hiddenCursor.position;
+        transform.RotateAround(startPoint.position, Vector3.up, vmrAngle);
+    }
+
+    // assign this function to run On Trial Begin
+    public void UpdateSettings(Trial trial)
+    {
+        // use settings.Get*() to access settings (independent variables)
+        vmrAngle = session.CurrentTrial.settings.GetFloat("vmr_angle");
+    }
+}
