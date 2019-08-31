cbe525c2fddcf46c1cba1a70bfb98670fe408405
Jack Brookes <jbrookes187@gmail.com>
2019-08-30 17:10:49 -0700

    part 11

---
+++ Assets/Scripts/InstructionsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UXF;
+
+// important! UI requires adding this namespace
+using UnityEngine.UI;
+
+public class InstructionsController : MonoBehaviour
+{   
+    public Text instructions;
+
+    void Awake()
+    {
+        instructions.text = ""; // no instructions until we start the session
+    }
+
+    // assign to On Session Begin event
+    public void Present(Session session)
+    {
+        instructions.text = session.settings.GetString("instruction");
+    }
+}
